383ca2796295bf1a90d3969535b675f0a2cde125
Kyas <107788758+Vryst@users.noreply.github.com>
2024-12-01 11:27:00 +0700

    Update classes.py
    Fixed:
     • Where battle log only shows the damage that monster dealt, now it shows players damage too :D
    
    Advancement:
     1. guard() method for the future plan

--- package/game/classes.py
+++ package/game/classes.py
@@ -2,6 +2,8 @@
 
 import os
 
+clear = os.system("clear")
+
 class Hero:
     
     def __init__(self,name,role,health,attack,defend,agility,crate,cdamage):
@@ -14,11 +16,17 @@ class Hero:
         self.crate = crate
         self.cdamage = cdamage
         self.guard = False
+        self.inventory = []
+        
         
     def Attack(attacker, target):
         os.system("clear")
         if target.health <= 0:
             print(f"{target.name} sudah mati :v")
+            
+            pass
+        if attacker.health <= 0:
+            print(f"{attacker.name} sudah mati :v")
             
             pass
             
@@ -29,9 +37,11 @@ class Hero:
             
             print(f"{attacker.name} HP = {attacker.health}\n{target.name} HP = {target.health}")
             print(f"\n\n{attacker.name} telah memberikan damage {attacker.attack} ke {target.name}")
+            print(f"\n{target.name} telah memberikan damage {target.attack} ke {attacker.name}")
             
     def guard(self):
         self.guard = True
+        print(f"Defend milik {self.name} telah meningkat 2x lipat!")
         
     def run():
     	return (True if random(range(1,10)) > 1 else False)
