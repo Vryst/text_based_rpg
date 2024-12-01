35d79e6c3fbe91f376c763b6966697410df29482
Kyas <107788758+Vryst@users.noreply.github.com>
2024-12-01 11:15:23 +0700

    Update view.py
    Optimizing functions placement

--- package/game/view.py
+++ package/game/view.py
@@ -1,81 +1,76 @@
 
 import os
 from classes import *
-
+from fun import *
 
 clear = os.system("clear")
 
             
 
-def percent(num=1):
-        return num/100
-        
 
-print(f"Selamat datang di Athanor :v\n\n\n")
-        
-hero_name = str(input("masukkan nama hero: "))
-hero_class = str(input("\npilih role :\n 1. Warrior\n 2. Mage\n 3. Archer\n\npilihan: "))
-
-if hero_class == "1":
-    hero = Hero(hero_name,
-    "warrior", # class
-    1000, # hp
-    130, # atk
-    30, # def
-    percent(15), # agi
-    35, # c.rate
-    percent(80) # c.dmg
-    )
+def main():
     
-if hero_class == "2":
-    hero = Hero(hero_name,
-    "mage", # class
-    400, # hp
-    270, # atk
-    15, # def
-    percent(10), # agi
-    10, # c.rate
-    percent(120) # c.dmg
-    )
+    global hero_class
+    global hero_name
+    global hero
     
-if hero_class == "3":
-    hero = Hero(hero_name,
-    "archer", # class
-    700, # hp
-    80, # atk
-    25, # def
-    percent(65), # agi
-    50, # c.rate
-    percent(100) # c.dmg
-    )
+    elmanuk = pilih_role()
+    hero_class = elmanuk[1]
+    hero_name = elmanuk[0]
+    
+    if hero_class == "1":
+       hero = Hero(hero_name,
+       "warrior", # class
+       1000, # hp
+       130, # atk
+       30, # def
+       percent(15), # agi
+       35, # c.rate
+       percent(80) # c.dmg
+       )
+       
+    if hero_class == "2":
+       hero = Hero(
+       hero_name,
+       "mage",  # class
+       400,     # hp
+       270,     # atk
+       15,      # def
+       percent(10),  # agi
+       10,      # c.rate
+       percent(120)   # c.dmg
+       )
 
-
-musuh = Hero("aob",
+    
+    
+    if hero_class == "3":
+       hero = Hero(hero_name,
+       "archer", # class
+       700, # hp
+       80, # atk
+       25, # def
+       percent(65), # agi
+       50, # c.rate
+       percent(100) # c.dmg
+       )
+    
+    
+    musuh = Hero("elmanuk",
     "warrior",
     1000,
-    130,
+    90,
     percent(20),
     percent(15),
     percent(35),
     percent(80)
     )
-on = True
-
-while on:
     
     
-    lanjut = input("Serang? (y/n) : ")
+    encounter(hero,musuh)
     
-    if lanjut == "y" or "Y" or "Yes" or "yes" or "Ya" or "ya":
-            
-            if hero.health or musuh.health <= 0:
-                pass
-            
-            
-            hero.Attack(musuh)
-            musuh.Attack(hero)
-    elif lanjut == "n":
-        
-        on = False
+    print("\n\napacoba")
+    
+    i = input("input apa saja untuk melanjutkan")
         
         
+main()
