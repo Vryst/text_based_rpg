dc52f6e61b235f1b9b6d0ea642f103b08ef0ab90
Kyas <107788758+Vryst@users.noreply.github.com>
2024-12-01 11:21:58 +0700

    Update fun.py
    Placing the previous role selection input from /view.py to there
    
    Adding encounter() function for future plan
    
    Fixing shop() function bugs after remodeling:
     • Variables error

--- package/game/fun.py
+++ package/game/fun.py
@@ -1,17 +1,113 @@
 
+import time
 from classes import *
-from view import *
 
 
-def role_picked(role):
+
+cls = os.system("clear")
+
+
+
+
+def pilih_role():
     
-    if role == "1":
-        return Hero(hero_name,100,10)
-         
-    if role == "2":
-        return Hero(hero_name,50,20)
-        
-    if role == "3":
-        return Hero(hero_name,70,15)
+    print(f"Selamat datang di Athanor :v\n\n\n")
+    
+    hero_name = str(input("masukkan nama hero: "))
+    hero_class = str(input("\npilih role :\n 1. Warrior\n 2. Mage\n 3. Archer\n\npilihan: "))
+    
+    return hero_name, hero_class
+
+
+
+
+def percent(num=1):
+        return num/100
         
 
+
+
+def encounter(hero,enemy):
+    
+    on = True
+    
+    while on:
+        
+        lanjut = input("Serang? (y/n) : ")
+        
+        if lanjut == "y" or "Y" or "Yes" or "yes" or "Ya" or "ya":
+                   
+                   if hero.health or enemy.health <= 0:
+                       
+                       pass
+                       
+                       hero.Attack(enemy)
+                       
+                       enemy.Attack(hero)
+                   if lanjut == "n":
+                       print("Game berakhir :v")
+                       break
+
+
+
+
+def shop(player,merchant):
+	print(f"Selamat datang di toko, orang asing! heheh..")
+	
+	
+	
+	if player.role == "thief":
+		print("\n1)Beli\n2)Jual\n3)Mencuri? heheheh :v")
+		pass
+	
+	
+	if player.role == "bandit":
+		print("\n1)Beli\n2)Jual\n3)Rampok? heheheh :v")
+		pass
+	
+	
+	if player.role == "hero":
+		print("\n1)Beli\n2)Jual\n3)Tunjukkan jasa?(menyombongkan diri dengan poin reputasi) :v")
+		pass
+	
+	
+	else:
+		print("\n1)Beli\n2)Jual\n3)Pergi")
+	
+	aksi = True
+	
+	while aksi:
+		
+		
+		
+		try:
+			global pilihan
+			pilihan = int(input("\nPilih(1,2,3) : "))
+		except:
+			pilihan = 0
+			print("Apakah kau bisa baca oh orang asing?")
+			#os.system("clear")
+			pass
+	
+		if pilihan == 1:
+			print("aku aob")
+			cls
+			break
+
+map = {
+
+"merchant" : "elmanuk"
+
+}
+
+'''p = Hero("Elmanuk",
+    "hero", # class
+    700, # hp
+    80, # atk
+    25, # def
+    percent(65), # agi
+    50, # c.rate
+    percent(100) # c.dmg
+    )'''
+    
+#shop(p,1)
